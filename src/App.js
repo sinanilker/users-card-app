23bc57fa0817e3d62f1301d1ba4f3abc0b6e066f
Sinan <sinan462@icloud.com>
2023-04-02 16:30:04 +0300

    Phase 1: Building and Creating each User Related Functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
+import PropTypes from 'prop-types';
+import Header from "./components/shared/header/Header";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //   Users Input State Hooks
+    const [user, setUser] = useState({userName: "", email: "", country: ""})
+    //   Error Checks
+    const [isUserValid, setIsUserValid] = useState({userName: true, email: true, country: true})
+    // Users List
+    const [userList, setUserList] = useState([])
+
+
+    axios.get("http://localhost:5050/users").then((response) => {
+        console.log(response.data);
+        userList.length === 0 && setUserList(response.data)
+    })
+
+    const addUser = (event) => {
+        event.preventDefault();
+        setIsUserValid({...user});
+        if (!(Object.values(user).some(value=> !value))) {
+            setUser(
+                [
+                    // Crypto to use randomized crypted Id for each
+                    ...userList, {Crypto.randomUUID, ...user}
+                ]
+            );
+            setUser({userName: "", email: "", country: ""});
+        }
+    }
+
+    const updateUserInput = (userProp) =>
+        setUser(prevState => ({...prevState, ...userProp}))
+
+    return (
+        <div className="App">
+    <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/shared/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = () => {
+    return (
+        <div>Header</div>
+    )
+}
+
+// Header.defaultProps = {
+//     logoTitle: "Header",
+// }
+
+export default Header
